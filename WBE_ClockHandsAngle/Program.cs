b4b23dd5ef5f25180f657bc91b049412a2da82a6
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-09 09:37:44 -0400

    added the logic that handles invalid entries

--- WBE_ClockHandsAngle/Program.cs
+++ WBE_ClockHandsAngle/Program.cs
@@ -17,28 +17,37 @@ namespace WBE_ClockHandsAngle
                     Console.Write("\nEnter the coordinate of the 1st circle's center\n\n>>> ");
                     string input = Console.ReadLine().Trim();
                     GetAngle(input);
-                    Console.ReadLine();
-                    Console.Clear();
                 }
                 catch (Exception ex)
                 {
                     Console.Clear();
                     Console.WriteLine(ex.Message);
                 }
+                Console.Write("\nPress Enter to try again...");
+                Console.ReadLine();
+                Console.Clear();
             } while (true);
         }
 
         private static void GetAngle(string input)
         {
+            double angle = -1;
             string[] arr = input.Split(':');
             int hHand = int.Parse(arr[0]);
             int mHand = int.Parse(arr[1]);
             // ensures that correct angle is going to be calculated even if the input is in 24-hour format
             // also marks the 12 o'clock as starting point for measuring angles 
             hHand = hHand >= 12 ? hHand - 12 : hHand;
-            double angle = mHand * 6 - (hHand * 30 + mHand * 1 / 2.0);
-            // assigns the value of the smaller angle formed by the hands to the variable angle
-            angle = angle > 180 ? 360 - angle : angle;
+            if (!(hHand >= 0 && hHand <= 12) || !(mHand >= 0 && mHand <= 59))
+            {
+                throw new Exception("\nInvalid Entry!");
+            }
+            else
+            {
+                angle = mHand * 6 - (hHand * 30 + mHand * 1 / 2.0);
+                // assigns the value of the smaller angle formed by the hands to the variable angle
+                angle = angle > 180 ? 360 - angle : angle;
+            }
             Console.WriteLine($"\nThe angle between the hands is {angle} degrees");
         }
     }
